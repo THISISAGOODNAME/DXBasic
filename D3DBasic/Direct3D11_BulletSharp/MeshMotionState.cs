92198f51246a08980f1fac45af551c45ad4634e9
THISISAGOODNAME <yangyj19931231@gmail.com>
2018-03-30 22:03:49 +0800

    bone & physics

---
+++ D3DBasic/Direct3D11_BulletSharp/MeshMotionState.cs
@@ -0,0 +1,30 @@
+﻿using BulletSharp;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Direct3D11_BulletSharp
+{
+    public class MeshMotionState: MotionState
+    {
+        public MeshRenderer Mesh { get; private set; }
+        public MeshMotionState(MeshRenderer mesh)
+        {
+            Mesh = mesh;
+        }
+
+        public override void GetWorldTransform(out BulletSharp.Math.Matrix worldTrans)
+        {
+
+            worldTrans = (Mesh.World * Matrix.Translation(Mesh.Mesh.Extent.Center)).ToBulletMatrix();
+        }
+
+        public override void SetWorldTransform(ref BulletSharp.Math.Matrix worldTrans)
+        {
+            Mesh.World = (Matrix.Translation(-Mesh.Mesh.Extent.Center).ToBulletMatrix() * worldTrans).ToSharpDXMatrix();
+        }
+    }
+}
---
+++ D3DBasic/Direct3D11_VertexSkinning/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Direct3D11_VertexSkinning
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+#if DEBUG
+            // Enable object tracking
+            SharpDX.Configuration.EnableObjectTracking = true;
+#endif
+            // Create the form to render to
+            var form = new Form1();
+            form.Text = "D3DRendering - Vertex Skinning";
+            form.ClientSize = new System.Drawing.Size(640, 480);
+            form.Show();
+            // Create and initialize the new D3D application
+            // Then run the application.
+            using (D3DApp app = new D3DApp(form))
+            {
+                app.VSync = false;
+                app.Initialize();
+                app.Run();
+            }
+        }
+    }
+}
